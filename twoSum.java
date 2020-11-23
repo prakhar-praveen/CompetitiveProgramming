686a0d6d9bfeae4b54997f6dc87356a4d4d40841
Prakhar Praveen <56411806+prakhar-praveen@users.noreply.github.com>
2020-11-23 02:50:12 -0700

    Create twoSum.java
    One-Pass Solution to the Famous TwoSum Problem

---
+++ twoSum.java
@@ -0,0 +1,22 @@
+class Solution 
+{
+    public int[] twoSum(int[] nums, int target) 
+    {
+    Map<Integer,Integer> hashTwoSum = new HashMap<>();
+    int arrayLength = nums.length;
+    for(int i = 0; i<arrayLength;i++) hashTwoSum.put(nums[i],i);
+    
+    int[] res = new int[2];
+    for(int i = 0;i<arrayLength;i++)
+    {  
+        int complement = target - nums[i];
+        if(hashTwoSum.containsKey(complement)&&hashTwoSum.get(complement)!=i)
+        {
+            res[0]=i;
+            res[1]=hashTwoSum.get(complement);
+        }
+    }
+return res;
+    }
+   
+}
